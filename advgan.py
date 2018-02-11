7e052ed0e9e32eef9803c21fe130f96251f25d4d
aria_rezaii <aria.rezaii@gmail.com>
2018-02-10 23:27:22 -0500

    Added option to create images from scratch

--- advgan.py
+++ advgan.py
@@ -300,9 +300,6 @@ class advGAN():
                     self.fake_images_sample, self.sample_noise = self.sampler(self.images)
             else:  # self.cgan_gen is True.
                 print 'Building images from scratch.'
-        # self.img_dim = self.opts.img_dim
-        # self.output_c_dim = self.opts.output_c_dim
-        # self.batch_size = self.opts.batch_size
                 self.added_noise = tf.random_normal(
                     [self.batch_size, self.img_dim, self.img_dim, self.output_c_dim], stddev=0.2)
                 self.noisy_images = tf.concat((self.images, self.added_noise), axis=3)
