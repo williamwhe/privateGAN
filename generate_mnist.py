99ce7f42c6b9c3fa012fc9a6ad438ab9c88d23dd
aria <arezaei@cs.stonybrook.edu>
2018-02-12 21:14:47 -0500

    Added more training for the discriminator

--- generate_mnist.py
+++ generate_mnist.py
@@ -142,6 +142,9 @@ def train():
             summary_str, D_loss, _ = sess.run(
                 [model.total_loss_merge_sum, model.d_loss, model.D_pre_train_op], feed)
             writer.add_summary(summary_str, iteration)
+            summary_str, D_loss, _ = sess.run(
+                [model.total_loss_merge_sum, model.d_loss, model.D_pre_train_op], feed)
+            writer.add_summary(summary_str, iteration)
 
             if iteration % opt.losses_log_every == 0:
                 print "iteration: ", iteration
